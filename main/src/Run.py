49db23f2955ef1bcc910fef349b48b0d7cadfd19
Musa-Sina-Ertugrul <yutubems@gmail.com>
2023-09-15 16:45:02 +0300

    Update for linting

--- main/src/Run.py
+++ main/src/Run.py
@@ -1,14 +1,25 @@
+"""
+Run class is an interface
+
+This insterface has been used by states,
+observers, gui and object
+"""
 from abc import ABCMeta, abstractmethod
+
 
 class Run(metaclass=ABCMeta):
 
+    """
+    Run class is an interface and facade
+    """
+
     @classmethod
     def __subclasshook__(cls, subclass):
-        return (hasattr(subclass, 'run') and 
-                callable(subclass.run) or 
-                NotImplemented)
-
+        return hasattr(subclass, "run") and callable(subclass.run) or NotImplemented
 
     @abstractmethod
     def run(self):
+        """
+        Command function to adjust final methods
+        """
         raise NotImplementedError
