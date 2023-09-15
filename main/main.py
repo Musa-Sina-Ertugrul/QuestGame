51b25f0dd76430b78f18d2084117eaf29d61ac46
Musa-Sina-Ertugrul <yutubems@gmail.com>
2023-09-15 16:27:27 +0300

    Update main.py for test

--- main/main.py
+++ main/main.py
@@ -1,14 +1,19 @@
+"""
+    Pass for linting
+"""
 import sys
-import pygame
+from pygame import pygame
 
 from src.flyweights.animated import Animated
-pygame.init()
 
-screen = pygame.display.set_mode((800,800))
+# pylint: disable=no-member
+pygame.init()
+# pylint: enable=no-member
+screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 while True:
     pygame.display.flip()
-    screen.fill((0,0,0))
-    screen.blit(Animated.data["crazy_chest"][0],(100,100))
+    screen.fill((0, 0, 0))
+    screen.blit(Animated.data["crazy_chest"][0], (100, 100))
     clock.tick(60)
     sys.exit()
