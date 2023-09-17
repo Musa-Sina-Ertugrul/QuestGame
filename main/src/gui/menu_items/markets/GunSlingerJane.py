7c13b591557dfe9d3b43cb7340e6d8ae3a18aa8c
Musa-Sina-Ertugrul <yutubems@gmail.com>
2023-09-17 19:10:07 +0300

    Start Market

---
+++ main/src/gui/menu_items/markets/GunSlingerJane.py
@@ -0,0 +1,155 @@
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+
+TODO: Update Docstrings
+
+"""
+
+from typing import NoReturn, Tuple, List
+from ..Market import Market
+
+class GunSlingerJane(Market):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
+    def __init__(self):
+        super().__init__()
+        super.elements = self._init_market()
+        self.items: List[List[int]] = [[]]  # TODO: Hardcode items
+
+    def buy_level(self, skill: int):
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        raise NotImplementedError
+
+    def reset_levels(self):
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        raise NotImplementedError
+
+    @property
+    def pos_x(self) -> int:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        return super().pos_x
+
+    @pos_x.setter
+    def pos_x(self, new_x: int) -> NoReturn:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        super().pos_x = new_x
+
+    @property
+    def pos_y(self) -> int:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        return super().pos_y
+
+    @pos_y.setter
+    def pos_y(self, new_y: int) -> NoReturn:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        super().pos_y = new_y
+
+    def relative_pos(self) -> Tuple[int, int]:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        raise NotImplementedError
+
+    def relative_height(self) -> int:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        raise NotImplementedError
+
+    def relative_width(self) -> int:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        raise NotImplementedError
+
+    def update_elements(self) -> NoReturn:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        raise NotImplementedError
+
+    def notify_states(self) -> NoReturn:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        raise NotImplementedError
+
+    def current_pos(self) -> Tuple[int, int]:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        return (self._x, self._y)
+
+    def run(self):
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        raise NotImplementedError
+
+    def update_skill(self):
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        raise NotImplementedError
