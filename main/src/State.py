5baddcd6ab5f5cccc8b327a3a456a6f647df12c5
Musa-Sina-Ertugrul <yutubems@gmail.com>
2023-09-17 19:09:32 +0300

    Add States

--- main/src/State.py
+++ main/src/State.py
@@ -27,3 +27,14 @@ class StateLookUpTable(Enum):
     START: EnumType = 2
     SHOP: EnumType = 3
     OPTIONS: EnumType = 4
+    ON_CLICK : EnumType = 5
+    CLICKED : EnumType = 6
+    ACTIVE: EnumType = 7
+    DEACTIVE: EnumType = 8
+    DYING: EnumType = 9
+    DEAD: EnumType = 10
+    SPAWNING: EnumType = 11
+    SCROLL_DOWN: EnumType = 12
+    SCROLL_UP : EnumType = 13
+    VISIBLE: EnumType = 14
+    NOT_VISIBLE: EnumType = 15
